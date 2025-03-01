61e04cb5f6cd580c5041b8e4a0be31c1218cb842
Persuasive Archives <mediapersuasive@gmail.com>
2025-03-01 19:48:28 +0800

    Support for Windows XP

--- index.c
+++ index.c
@@ -1,43 +1,38 @@
 #include <windows.h>
 
-
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
-   
     const char CLASS_NAME[]  = "Sample Window Class";
     
     WNDCLASS wc = {0};
-    wc.lpfnWndProc   = WindowProc;      
-    wc.hInstance     = hInstance;       
-    wc.lpszClassName = CLASS_NAME;     
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
     
-    // Register the window class
     if (!RegisterClass(&wc))
     {
-        return -1;  
+        return -1;
     }
 
-    
     HWND hwnd = CreateWindowEx(
         0,                     
         CLASS_NAME,            
         "Virus Alert !",             
-        WS_OVERLAPPEDWINDOW,   // Window style
+        WS_OVERLAPPEDWINDOW,   
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL,                  // Parent window
-        NULL,                  // Menu
-        hInstance,             // Instance handle
-        NULL                   // Additional application data
+        NULL,                  
+        NULL,                  
+        hInstance,            
+        NULL                   
     );
 
     if (hwnd == NULL)
     {
-        return -1; 
+        return -1;
     }
 
-   
     CreateWindow("BUTTON", "Yes", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
                  50, 150, 100, 30, hwnd, (HMENU)1, hInstance, NULL);
     CreateWindow("BUTTON", "No", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
@@ -45,10 +40,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
     CreateWindow("BUTTON", "Cancel", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
                  270, 150, 100, 30, hwnd, (HMENU)3, hInstance, NULL);
 
-
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
-
 
     MSG msg = {0};
     while (GetMessage(&msg, NULL, 0, 0))
@@ -60,14 +53,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, 
     return (int) msg.wParam;
 }
 
-
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
     case WM_CREATE:
-
-        CreateWindow("STATIC", "Example Text", WS_VISIBLE | WS_CHILD | SS_CENTER,
+        CreateWindow("STATIC", "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus", 
+                     WS_VISIBLE | WS_CHILD | SS_CENTER,
                      50, 50, 300, 50, hwnd, (HMENU)0, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
         break;
     
@@ -77,7 +69,6 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         case 1: 
         case 2: 
         case 3: 
-            
             PostMessage(hwnd, WM_CLOSE, 0, 0);
             break;
         }
