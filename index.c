3663b635b34b076e9527fe528a22e9ccc8099aa0
Persuasive Archives <mediapersuasive@gmail.com>
2025-04-06 13:22:16 +0800

    Update index.c

--- index.c
+++ index.c
@@ -1,46 +1,52 @@
 #define _WIN32_WINNT 0x0501
 
 #include <windows.h>
+#include <stdio.h> // For sprintf
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
-    const char CLASS_NAME[]  = "Sample Window Class";
-    
+    const char CLASS_NAME[]  = "GenericErrorClass";
+    char errorTitle[256];
+    char errorMessage[512];
+
     WNDCLASS wc = {0};
     wc.lpfnWndProc   = WindowProc;
     wc.hInstance     = hInstance;
     wc.lpszClassName = CLASS_NAME;
-    
+
     if (!RegisterClass(&wc))
     {
+        sprintf(errorTitle, "Error");
+        sprintf(errorMessage, "Failed to register window class. Error code: %lu", GetLastError());
+        MessageBox(NULL, errorMessage, errorTitle, MB_OK | MB_ICONERROR);
         return -1;
     }
 
     HWND hwnd = CreateWindowEx(
-        0,                     
-        CLASS_NAME,            
-        "Virus Alert !",             
-        WS_OVERLAPPEDWINDOW,   
+        0,
+        CLASS_NAME,
+        "An Error Has Occurred", // Generic title
+        WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL,                  
-        NULL,                  
-        hInstance,            
-        NULL                   
+        NULL,
+        NULL,
+        hInstance,
+        NULL
     );
 
     if (hwnd == NULL)
     {
+        sprintf(errorTitle, "Error");
+        sprintf(errorMessage, "Failed to create window. Error code: %lu", GetLastError());
+        MessageBox(NULL, errorMessage, errorTitle, MB_OK | MB_ICONERROR);
         return -1;
     }
 
-    CreateWindow("BUTTON", "Yes", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                 50, 150, 100, 30, hwnd, (HMENU)1, hInstance, NULL);
-    CreateWindow("BUTTON", "No", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                 160, 150, 100, 30, hwnd, (HMENU)2, hInstance, NULL);
-    CreateWindow("BUTTON", "Cancel", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-                 270, 150, 100, 30, hwnd, (HMENU)3, hInstance, NULL);
+    // Customize button text and message based on the specific error if needed
+    CreateWindow("BUTTON", "OK", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+               150, 150, 100, 30, hwnd, (HMENU)1, hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
@@ -60,17 +66,15 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     switch (uMsg)
     {
     case WM_CREATE:
-        CreateWindow("STATIC", "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards, Albanian virus", 
-                     WS_VISIBLE | WS_CHILD | SS_CENTER,
-                     50, 50, 300, 50, hwnd, (HMENU)0, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
+        CreateWindow("STATIC", "A generic error has occurred. Please try again or contact support if the issue persists.",
+                   WS_VISIBLE | WS_CHILD | SS_CENTER | SS_WORDBREAK,
+                   50, 50, 300, 80, hwnd, (HMENU)0, ((LPCREATESTRUCT)lParam)->hInstance, NULL);
         break;
-    
+
     case WM_COMMAND:
         switch (LOWORD(wParam))
         {
-        case 1: 
-        case 2: 
-        case 3: 
+        case 1: // ID for the "OK" button
             PostMessage(hwnd, WM_CLOSE, 0, 0);
             break;
         }
@@ -85,5 +89,3 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     }
     return 0;
 }
-
-
